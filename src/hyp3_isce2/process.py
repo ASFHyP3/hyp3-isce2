18fbbc7954e58f5b59d3ba4e29f23d4e68eb745c
Forrest Williams <forrestfwilliams@icloud.com>
2023-03-27 14:57:50 +0000

    process: change function name

--- src/hyp3_isce2/process.py
+++ src/hyp3_isce2/process.py
@@ -16,7 +16,7 @@ log = logging.getLogger(__name__)
 TOPSAPP = str(Path(os.getenv('ISCE_HOME')) / 'applications' / 'topsApp.py')
 
 
-def process_topsapp_burst_ifg(arg_list: Iterable[str] = ['-h']) -> None:
+def topsapp_burst(arg_list: Iterable[str] = ['-h']) -> None:
     """Create a burst interferogram
 
     Args:
@@ -30,13 +30,13 @@ def process_topsapp_burst_ifg(arg_list: Iterable[str] = ['-h']) -> None:
 def main():
     """process_isce2 entrypoint"""
     parser = argparse.ArgumentParser(
-        prog='process_topsapp_burst_ifg',
+        prog='topsapp_burst',
         description=__doc__,
     )
     parser.add_argument('--version', action='version', version=f'%(prog)s {__version__}')
     args = parser.parse_args()
 
-    process_topsapp_burst_ifg(**args.__dict__)
+    topsapp_burst(**args.__dict__)
 
 
 if __name__ == "__main__":
