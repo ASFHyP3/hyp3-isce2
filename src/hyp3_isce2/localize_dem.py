f4d1dde1cfb725031ad91c9900ff50aea2551199
Jake Herrmann <jtherrmann@alaska.edu>
2023-04-03 09:59:29 -0800

    credit original source code

--- src/hyp3_isce2/localize_dem.py
+++ src/hyp3_isce2/localize_dem.py
@@ -1,6 +1,17 @@
-# TODO: give credit to
-# https://github.com/ACCESS-Cloud-Based-InSAR/DockerizedTopsApp/blob/dev/isce2_topsapp/localize_dem.py
-# and link to the repo's license (may want to read the license terms more carefully)
+# Copyright 2021-present Caltech
+# Modifications Copyright 2023 Alaska Satellite Facility
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import site
 import subprocess
