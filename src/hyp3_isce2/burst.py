0622e74c3414f946cef8a3d5a12df1f79e4cb901
Forrest Williams <ffwilliams2@alaska.edu>
2023-04-03 09:26:50 -0500

    add docstrings and typing

--- src/hyp3_isce2/burst.py
+++ src/hyp3_isce2/burst.py
@@ -4,11 +4,17 @@ import time
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Iterator, List, Tuple, Union
+from typing import (
+    Iterator,
+    List,
+    Tuple,
+    Union
+)
 
 import pandas as pd
 import requests
 from shapely import geometry
+
 
 URL = 'https://sentinel1-burst.asf.alaska.edu'
 
@@ -24,6 +30,8 @@ class BurstParams:
 
 
 class BurstMetadata:
+    """Class that contains the metadata for a burst."""
+
     def __init__(self, metadata: ET.Element, burst_params: BurstParams):
         self.safe_name = burst_params.granule
         self.swath = burst_params.swath
@@ -56,20 +64,41 @@ class BurstMetadata:
         self.orbit_direction = self.manifest.findtext('.//{*}pass').lower()
 
     @staticmethod
-    def reformat_gcp(point):
+    def reformat_gcp(point: ET.Element) -> dict:
+        """Reformat a burst geolocation grid point to a dictionary.
+
+        Args:
+            point: The geolocation grid point to reformat.
+
+        Returns:
+            A dictionary containing the geolocation grid point's line, pixel, latitude, longitude, and height.
+        """
         attribs = ['line', 'pixel', 'latitude', 'longitude', 'height']
         values = {}
         for attrib in attribs:
             values[attrib] = float(point.find(attrib).text)
         return values
 
-    def create_gcp_df(self):
+    def create_gcp_df(self) -> pd.DataFrame:
+        """Create a dataframe of geolocation grid points.
+
+        Returns:
+            A dataframe containing the geolocation grid points for the burst.
+        """
         points = self.annotation.findall('.//{*}geolocationGridPoint')
         gcp_df = pd.DataFrame([self.reformat_gcp(x) for x in points])
         gcp_df = gcp_df.sort_values(['line', 'pixel']).reset_index(drop=True)
         return gcp_df
 
-    def create_geometry(self, gcp_df):
+    def create_geometry(self, gcp_df: pd.DataFrame) -> Tuple[geometry.Polygon, Tuple[float], Tuple[float]]:
+        """Create a shapely polygon and centroid for the burst.
+
+        Args:
+            gcp_df: A dataframe containing the geolocation grid points for the burst.
+
+        Returns:
+            A tuple containing the shapely polygon, bounding box, and centroid for the burst.
+        """
         burst_index = self.burst_number - 1
         lines = int(self.annotation.findtext('.//{*}linesPerBurst'))
         first_line = gcp_df.loc[gcp_df['line'] == burst_index * lines, ['longitude', 'latitude']]
@@ -87,15 +116,32 @@ class BurstMetadata:
         return footprint, footprint.bounds, centroid
 
 
-def create_burst_request(params: BurstParams, content: str) -> dict:
+def create_burst_request_url(params: BurstParams, content: str) -> str:
+    """Create a URL to request a burst from the API.
+
+    Args:
+        params: The burst search parameters.
+
+    Returns:
+        A URL to request a burst from the API.
+    """
     filetypes = {'metadata': 'xml', 'geotiff': 'tiff'}
     extension = filetypes[content]
     burst_number_zero_indexed = params.burst_number - 1
     url = f'{URL}/{params.granule}/{params.swath}/{params.polarization}/{burst_number_zero_indexed}.{extension}'
-    return {'url': url}
+    return url
 
 
 def wait_for_extractor(response: requests.Response, sleep_time: int = 15) -> bool:
+    """Wait for the burst extractor to finish processing a burst.
+
+    Args:
+        response: The response from the burst extractor.
+        sleep_time: The number of seconds to wait between checking the status of the burst.
+
+    Returns:
+        True if the burst was successfully downloaded, False otherwise.
+    """
     if response.status_code == 202:
         time.sleep(15)
         return False
@@ -104,14 +150,24 @@ def wait_for_extractor(response: requests.Response, sleep_time: int = 15) -> boo
     return True
 
 
-def download_from_extractor(asf_session: requests.Session, burst_params: BurstParams, content: str):
+def download_from_extractor(asf_session: requests.Session, burst_params: BurstParams, content: str) -> bytes:
+    """Download burst data from the extractor.
+
+    Args:
+        asf_session: A requests session with an ASF URS cookie.
+        burst_params: The burst search parameters.
+        content: The type of content to download (metadata or geotiff).
+
+    Returns:
+        The downloaded content.
+    """
     burst_request = {
-        'url': get_burst_request_url(burst_params, content=content),
-        'cookies': {'asf-urs': asf_session.cookies['asf-urs']}
+        'url': create_burst_request_url(burst_params, content=content),
+        'cookies': {'asf-urs': asf_session.cookies['asf-urs']},
     }
 
-    for ii in range(1, 11):
-        print(f'Download attempt #{ii}')
+    for i in range(1, 11):
+        print(f'Download attempt #{i}')
         response = asf_session.get(**burst_request)
         downloaded = wait_for_extractor(response)
         if downloaded:
@@ -124,6 +180,16 @@ def download_from_extractor(asf_session: requests.Session, burst_params: BurstPa
 
 
 def download_metadata(asf_session: requests.Session, burst_params: BurstParams, out_file: Union[Path, str] = None):
+    """Download burst metadata.
+
+    Args:
+        asf_session: A requests session with an ASF URS cookie.
+        burst_params: The burst search parameters.
+        out_file: The path to save the metadata to (if desired).
+
+    Returns:
+        The metadata as an ElementTree object or the path to the saved metadata file.
+    """
     content = download_from_extractor(asf_session, burst_params, 'metadata')
     metadata = ET.fromstring(content)
 
@@ -136,7 +202,17 @@ def download_metadata(asf_session: requests.Session, burst_params: BurstParams, 
     return str(out_file)
 
 
-def download_burst(asf_session: requests.Session, burst_params: BurstParams, out_file: Union[Path, str]):
+def download_burst(asf_session: requests.Session, burst_params: BurstParams, out_file: Union[Path, str]) -> str:
+    """Download a burst geotiff.
+
+    Args:
+        asf_session: A requests session with an ASF URS cookie.
+        burst_params: The burst search parameters.
+        out_file: The path to save the geotiff to (if desired).
+
+    Returns:
+        The path to the saved geotiff file.
+    """
     content = download_from_extractor(asf_session, burst_params, 'geotiff')
 
     with open(out_file, 'wb') as f:
@@ -146,7 +222,9 @@ def download_burst(asf_session: requests.Session, burst_params: BurstParams, out
 
 
 def spoof_safe(asf_session: requests.Session, burst: BurstMetadata, base_path: Path = Path('.')) -> Path:
-    """Creates this file structure:
+    """Spoof a Sentinel-1 SAFE file for a burst.
+
+    The created SAFE file will be saved to the base_path directory. The SAFE will have the following structure:
     SLC.SAFE/
     ├── manifest.safe
     ├── measurement/
@@ -156,6 +234,14 @@ def spoof_safe(asf_session: requests.Session, burst: BurstMetadata, base_path: P
         └── calibration/
             ├── calibration.xml
             └── noise.xml
+
+    Args:
+        asf_session: A requests session with an ASF URS cookie.
+        burst: The burst metadata.
+        base_path: The path to save the SAFE file to.
+
+    Returns:
+        The path to the saved SAFE file.
     """
     safe_path = base_path / f'{burst.safe_name}.SAFE'
     annotation_path = safe_path / 'annotation'
@@ -179,9 +265,20 @@ def spoof_safe(asf_session: requests.Session, burst: BurstMetadata, base_path: P
 
 
 # TODO currently only validated for descending orbits
-def get_region_of_interest(
-    poly1: geometry.Polygon, poly2: geometry.Polygon, is_ascending: bool = True
-) -> Tuple[float, float, float, float]:
+def get_region_of_interest(poly1: geometry.Polygon, poly2: geometry.Polygon, is_ascending: bool = True) -> Tuple[float]:
+    """Get the region of interest for two bursts that will lead to single burst ISCE2 processing.
+
+    For a descending orbit, the roi is in the lower left corner of the two bursts, and for an ascending orbit the roi is
+    in the upper right corner.
+
+    Args:
+        poly1: The first burst's footprint.
+        poly2: The second burst's footprint.
+        is_ascending: Whether the orbit is ascending or descending.
+
+    Returns:
+        The region of interest as a tuple of (minx, miny, maxx, maxy).
+    """
     bbox1 = geometry.box(*poly1.bounds)
     bbox2 = geometry.box(*poly2.bounds)
     intersection = bbox1.intersection(bbox2)
@@ -189,12 +286,18 @@ def get_region_of_interest(
 
     x, y = (0, 1) if is_ascending else (2, 1)
     roi = geometry.Point(bounds[x], bounds[y]).buffer(0.005)
-    return roi.bounds  # returns (minx, miny, maxx, maxy)
+    return roi.bounds
 
 
 def get_asf_session() -> requests.Session:
-    # requests will automatically use the netrc file:
-    # https://requests.readthedocs.io/en/latest/user/authentication/#netrc-authentication
+    """Get a requests session with an ASF URS cookie.
+
+    requests will automatically use the netrc file:
+    https://requests.readthedocs.io/en/latest/user/authentication/#netrc-authentication
+
+    Returns:
+        A requests session with an ASF URS cookie.
+    """
     session = requests.Session()
     payload = {
         'response_type': 'code',
@@ -207,13 +310,20 @@ def get_asf_session() -> requests.Session:
 
 
 def download_bursts(param_list: Iterator[BurstParams]) -> List[BurstMetadata]:
-    """Steps
+    """Download bursts and create SAFE files.
+
     For each burst:
         1. Download metadata
         2. Create BurstMetadata object
         3. Create directory structure
         4. Write metadata
         5. Download and write geotiff
+
+    Args:
+        param_list: An iterator of burst search parameters.
+
+    Returns:
+        A list of BurstMetadata objects.
     """
     bursts = []
     with get_asf_session() as asf_session:
@@ -227,9 +337,3 @@ def download_bursts(param_list: Iterator[BurstParams]) -> List[BurstMetadata]:
     print('SAFEs created!')
 
     return bursts
-
-
-if __name__ == '__main__':
-    burst_params1 = BurstParams('S1A_IW_SLC__1SDV_20211229T231926_20211229T231953_041230_04E66A_3DBE', 'IW2', 'VV', 3)
-    burst_params2 = BurstParams('S1A_IW_SLC__1SDV_20220110T231926_20220110T231953_041405_04EC57_103E', 'IW2', 'VV', 3)
-    metadata = download_bursts([burst_params1, burst_params2])
