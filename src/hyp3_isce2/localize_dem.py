47fb30463c0b01dfedfb8d719240aefd62f25a79
Jake Herrmann <jtherrmann@alaska.edu>
2023-04-05 11:16:08 -0800

    factor out buffer_extent function

--- src/hyp3_isce2/localize_dem.py
+++ src/hyp3_isce2/localize_dem.py
@@ -48,12 +48,24 @@ def fix_image_xml(isce_raster_path: str) -> str:
     return isce_raster_path
 
 
+def buffer_extent(extent: list, buffer: float) -> list:
+    extent_geo = box(*extent)
+    extent_buffered = list(extent_geo.buffer(buffer).bounds)
+    return [
+        np.floor(extent_buffered[0]),
+        np.floor(extent_buffered[1]),
+        np.ceil(extent_buffered[2]),
+        np.ceil(extent_buffered[3])
+    ]
+
+
 def download_dem_for_isce2(
         extent: list,
         dem_name: str = 'glo_30',
         dem_dir: Path = None,
         buffer: float = .4) -> dict:
     # FIXME: second ymin should be ymax in `extent` description; open PR to original repo as well
+    # TODO: update docstr format
     """
     Parameters
     ----------
@@ -71,14 +83,7 @@ def download_dem_for_isce2(
     dem_dir = dem_dir or Path('.')
     dem_dir.mkdir(exist_ok=True, parents=True)
 
-    extent_geo = box(*extent)
-    extent_buffered = list(extent_geo.buffer(buffer).bounds)
-    extent_buffered = [
-        np.floor(extent_buffered[0]),
-        np.floor(extent_buffered[1]),
-        np.ceil(extent_buffered[2]),
-        np.ceil(extent_buffered[3])
-    ]
+    extent_buffered = buffer_extent(extent, buffer)
 
     dem_res = 0.0002777777777777777775
     dem_array, dem_profile = stitch_dem(
