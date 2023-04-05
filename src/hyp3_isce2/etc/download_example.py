1b30253c6c7cfff188d9b56a1902f5115c3c167c
Forrest Williams <forrestfwilliams@icloud.com>
2023-04-05 13:34:31 +0000

    fix imports

--- src/hyp3_isce2/etc/download_example.py
+++ src/hyp3_isce2/etc/download_example.py
@@ -1,4 +1,10 @@
-from hyp3_isce2.burst import BurstParams, get_asf_session, download_metadata
+from hyp3_isce2.burst import (
+    BurstParams,
+    download_metadata,
+    get_asf_session
+)
+
+
 ref_desc = BurstParams('S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85', 'IW2', 'VV', 3)
 sec_desc = BurstParams('S1A_IW_SLC__1SDV_20200616T022252_20200616T022319_033036_03D3A3_5D11', 'IW2', 'VV', 3)
 ref_asc = BurstParams('S1A_IW_SLC__1SDV_20211229T231926_20211229T231953_041230_04E66A_3DBE', 'IW1', 'VV', 4)
