5127170db980eed824fed859feb4fac8c3ab460a
Forrest Williams <forrestfwilliams@icloud.com>
2023-04-05 20:48:20 +0000

    refactor aux_cal

--- src/hyp3_isce2/s1_auxcal.py
+++ src/hyp3_isce2/s1_auxcal.py
@@ -14,35 +14,34 @@
 # limitations under the License.
 
 import zipfile
+from io import BytesIO
 from pathlib import Path
 from typing import Union
 
 import requests
 
+
 S1A_AUX_URL = 'https://sar-mpc.eu/download/55282da1-679d-4ecf-aeef-d06b024451cf'
 S1B_AUX_URL = 'https://sar-mpc.eu/download/3c8b7c8d-d3de-4381-a19d-7611fb8734b9'
 
 
-def download_aux_cal(aux_cal_dir: Union[str, Path] = None):
-    aux_cal_dir = aux_cal_dir or 'aux_cal'
-    aux_cal_dir = Path(aux_cal_dir)
+def download_platform(url, aux_cal_dir):
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    content = BytesIO(resp.content)
+    with zipfile.ZipFile(content) as zip_file:
+        zip_file.extractall(aux_cal_dir)
+
+
+def download_aux_cal(aux_cal_dir: Union[str, Path] = 'aux_cal'):
+    if not isinstance(aux_cal_dir, Path):
+        aux_cal_dir = Path(aux_cal_dir)
+
     aux_cal_dir.mkdir(exist_ok=True, parents=True)
+    for url in [S1A_AUX_URL, S1B_AUX_URL]:
+        download_platform(url, aux_cal_dir)
 
-    def download_one(url):
-        resp = requests.get(url)
-        file_name = url.split('/')[-1]
-        out_path = aux_cal_dir/file_name
 
-        with open(out_path, 'wb') as file:
-            file.write(resp.content)
-        return out_path
-
-    s1a_path = download_one(S1A_AUX_URL)
-    s1b_path = download_one(S1B_AUX_URL)
-
-    with zipfile.ZipFile(s1a_path) as zip_file:
-        zip_file.extractall(aux_cal_dir)
-    with zipfile.ZipFile(s1b_path) as zip_file:
-        zip_file.extractall(aux_cal_dir)
-
-    return {'aux_cal_dir': str(aux_cal_dir)}
+if __name__ == '__main__':
+    download_aux_cal()
