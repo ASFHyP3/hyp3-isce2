80652f37fae0237f28aa9aa5ae801e5c3f284f4c
Andrew Player <andrewplayer3@gmail.com>
2023-04-07 13:50:34 -0500

    test contents of file, rather than existance

--- tests/test_topsapp.py
+++ tests/test_topsapp.py
@@ -11,15 +11,17 @@ def test_topsapp_burst_config(tmp_path):
         dem_filename='dem.tif',
         swath=1,
     )
-    template = config.generate_template()
-    assert 'S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.SAFE' in template
-    assert 'S1A_IW_SLC__1SDV_20200616T022252_20200616T022319_033036_03D3A3_5D11.SAFE' in template
-    assert 'orbits' in template
-    assert 'aux_cal' in template
-    assert 'dem.tif' in template
-    assert '[-118.0, 37.0, -117.0, 38.0]' in template
-    assert '[1]' in template
 
     template_path = tmp_path / 'topsapp.xml'
     config.write_template(template_path)
     assert template_path.exists()
+    
+    with open(template_path, 'r') as template_file:
+        template = template_file.read()
+        assert 'S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.SAFE' in template
+        assert 'S1A_IW_SLC__1SDV_20200616T022252_20200616T022319_033036_03D3A3_5D11.SAFE' in template
+        assert 'orbits' in template
+        assert 'aux_cal' in template
+        assert 'dem.tif' in template
+        assert '[-118.0, 37.0, -117.0, 38.0]' in template
+        assert '[1]' in template
