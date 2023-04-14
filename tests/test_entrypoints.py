43059b64bb796fd232b343b48a882ff14d7e5a40
Forrest Williams <ffwilliams2@alaska.edu>
2023-04-14 13:53:09 -0500

    add new entrypoint tests

--- tests/test_entrypoints.py
+++ tests/test_entrypoints.py
@@ -1,3 +1,13 @@
 def test_hyp3_isce2(script_runner):
     ret = script_runner.run('python', '-m', 'hyp3_isce2', '-h')
     assert ret.success
+
+
+def test_insar_tops_burst(script_runner):
+    ret = script_runner.run('python', '-m', 'hyp3_isce2', 'insart_tops_burst', '-h')
+    assert ret.success
+
+
+def test_insar_tops_burst_main(script_runner):
+    ret = script_runner.run('insar_tops_burst', '-h')
+    assert ret.success
