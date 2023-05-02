7050206277098f9d64a2a0b8ceaa062ceb2ef59e
mfangaritav <mf.angarita52@gmail.com>
2023-05-02 00:33:43 +0000

    Adding stripmapapp function

--- src/hyp3_isce2/insar_stripmap.py
+++ src/hyp3_isce2/insar_stripmap.py
@@ -9,8 +9,17 @@ import site
 import sys
 from pathlib import Path
 
+from hyp3_isce2 import stripmapapp_alos as stripmapapp
+
 from hyp3lib.aws import upload_file_to_s3
 from hyp3lib.image import create_thumbnail
+from hyp3_isce2.dem import download_dem_for_isce2
+import zipfile
+import glob
+import os
+
+import asf_search as asf
+from shapely.geometry.polygon import Polygon
 
 
 log = logging.getLogger(__name__)
@@ -22,7 +31,7 @@ if str(ISCE_APPLICATIONS) not in os.environ['PATH'].split(os.pathsep):
     os.environ['PATH'] = str(ISCE_APPLICATIONS) + os.pathsep + os.environ['PATH']
 
 
-def insar_stripmap(reference_scene: str, secondary_scene: str) -> Path:
+def insar_stripmap(user: str, password: str, reference_scene: str, secondary_scene: str) -> Path:
     """Create an interferogram
 
     This is a placeholder function. It will be replaced with your actual scientific workflow.
@@ -34,11 +43,58 @@ def insar_stripmap(reference_scene: str, secondary_scene: str) -> Path:
     Returns:
         Path to the output files
     """
+    session = asf.ASFSession().auth_with_creds(user, password)
+    
+    results = asf.granule_search([reference_scene,secondary_scene])
+    
+    polys=[]
+    durls=[]
+    for result in results:
+        if 'L1.0' in result.properties['url']:
+            polys.append(Polygon(results[0].geometry['coordinates'][0]))
+            durls.append(result.properties['url'])
+        
+    for i in range(len(polys)):
+        if i==0:
+            intersection=polys[i].intersection(polys[i+1])
+        else:
+            intersection=polys[i].intersection(intersection)
+    
+    dem_dir = Path('dem')
+    dem_path = download_dem_for_isce2(intersection.bounds, dem_name='glo_30', dem_dir=dem_dir, buffer=0)
+    
+    insar_roi=intersection.bounds
+    asf.download_urls(urls=durls, path='./', session=session,processes=2)
+    
+    zips=glob.glob('*.zip')
+    for i,zipf in enumerate(sorted(zips[0:2])):
+        with zipfile.ZipFile(zipf, 'r') as zip_ref:
+            zip_ref.extractall('./')
+        
+        if i==0:
+            reference_image=glob.glob('./'+zipf.split('.zip')[0]+'/IMG-*')[0]
+            reference_leader=glob.glob('./'+zipf.split('.zip')[0]+'/LED-*')[0]
+        else:
+            secondary_image=glob.glob('./'+zipf.split('.zip')[0]+'/IMG-*')[0]
+            secondary_leader=glob.glob('./'+zipf.split('.zip')[0]+'/LED-*')[0]
+        
+        os.remove(zipf)
+    
+    config = stripmapapp.StripmapappConfig(
+        reference_image=reference_image,
+        reference_leader=reference_leader,
+        secondary_image=secondary_image,
+        secondary_leader=secondary_leader,
+        roi=insar_roi,
+        dem_filename=str(dem_path),
+    )
+    config_path = config.write_template('stripmapApp.xml')
+    
+    stripmapapp.run_stripmapapp(start='startup', end='geocode', config_xml=config_path)
+    #raise NotImplementedError('This is a placeholder function. Replace it with your actual scientific workflow.')
 
-    raise NotImplementedError('This is a placeholder function. Replace it with your actual scientific workflow.')
-
-    product_file = Path("product_file_name.zip")
-    return product_file
+    #product_file = Path("product_file_name.zip")
+    return Path('interferogram')
 
 
 def main():
@@ -47,6 +103,8 @@ def main():
 
     parser.add_argument('--bucket', type=str, default='', help='AWS S3 bucket HyP3 for upload the final product(s)')
     parser.add_argument('--bucket-prefix', type=str, default='', help='Add a bucket prefix to product(s)')
+    parser.add_argument('--username', type=str, required=True)
+    parser.add_argument('--password', type=str, required=True)
     parser.add_argument('--reference-scene', type=str, required=True)
     parser.add_argument('--secondary-scene', type=str, required=True)
 
@@ -56,6 +114,8 @@ def main():
     log.debug(' '.join(sys.argv))
 
     product_file = insar_stripmap(
+        user=args.username,
+        password=args.password,
         reference_scene=args.reference_scene,
         secondary_scene=args.secondary_scene,
     )
@@ -63,6 +123,7 @@ def main():
     log.info('InSAR Stripmap run completed successfully')
 
     if args.bucket:
+        base_name = f'{reference_scene}x{secondary_scene}'
         upload_file_to_s3(product_file, args.bucket, args.bucket_prefix)
         browse_images = product_file.with_suffix('.png')
         for browse in browse_images:
