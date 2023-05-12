92406a6b021400364623d9dd87c3da2d682eca40
jiangzhu <jzhu4@alaska.edu>
2023-05-11 17:18:55 -0800

    modify make_tiff function

--- src/hyp3_isce2/insar_tops_burst.py
+++ src/hyp3_isce2/insar_tops_burst.py
@@ -8,6 +8,7 @@ import sys
 from pathlib import Path
 from shutil import make_archive
 from osgeo import gdal
+from osgeo import osr
 
 from hyp3lib.aws import upload_file_to_s3
 from hyp3lib.get_orb import downloadSentinelOrbitFile
