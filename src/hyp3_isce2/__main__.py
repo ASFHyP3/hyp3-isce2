e46c3c8eb05748584cb967924ba356d1081da294
Forrest Williams <ffwilliams2@alaska.edu>
2023-05-15 13:43:17 -0500

    add ability to specify credentials as environment variables

--- src/hyp3_isce2/__main__.py
+++ src/hyp3_isce2/__main__.py
@@ -2,8 +2,11 @@
 ISCE2 processing for HyP3
 """
 import argparse
+import os
 import sys
 from importlib.metadata import entry_points
+
+from hyp3lib.fetch import write_credentials_to_netrc_file
 
 
 def main():
@@ -14,15 +17,19 @@ def main():
     parser = argparse.ArgumentParser(prefix_chars='+', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
         '++process',
-        choices=[
-            'insar_tops_burst',
-            'insar_stripmap'
-        ],
+        choices=['insar_tops_burst', 'insar_stripmap'],
         default='insar_tops_burst',
         help='Select the HyP3 entrypoint to use',  # HyP3 entrypoints are specified in `pyproject.toml`
     )
 
     args, unknowns = parser.parse_known_args()
+
+    # NOTE: Earthdata username and password should be provided as environment variables, or be present in your .netrc
+    username = os.getenv('EARTHDATA_USERNAME')
+    password = os.getenv('EARTHDATA_PASSWORD')
+    if username and password:
+        write_credentials_to_netrc_file(username, password, append=False)
+
     # NOTE: Cast to set because of: https://github.com/pypa/setuptools/issues/3649
     # NOTE: Will need to update to `entry_points(group='hyp3', name=args.process)` when updating to python 3.10
     eps = entry_points()['hyp3']
