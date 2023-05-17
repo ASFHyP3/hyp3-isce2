892878c61c43f9d9ca6a0f2504e2ea4017fa3fd1
Forrest Williams <ffwilliams2@alaska.edu>
2023-05-17 15:09:59 -0500

    add tests for extent_from_geotransform

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from hyp3_isce2.utils import utm_from_lon_lat
+from hyp3_isce2.utils import utm_from_lon_lat, extent_from_geotransform
 
 
 def test_utm_from_lon_lat():
@@ -11,4 +11,7 @@ def test_utm_from_lon_lat():
     assert utm_from_lon_lat(-360, -1) == 32731
 
 
-# TODO: test extent_from_geotransform
+def test_extent_from_geotransform():
+    assert extent_from_geotransform((0, 1, 0, 0, 0, -1), 1, 1) == (0, 0, 1, -1)
+    assert extent_from_geotransform((0, 1, 0, 0, 0, -1), 2, 2) == (0, 0, 2, -2)
+    assert extent_from_geotransform((0, 1, 0, 0, 0, -1), 1, 3) == (0, 0, 1, -3)
