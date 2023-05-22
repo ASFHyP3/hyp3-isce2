07dedcd7eb8918b135ef648e26a3c0f6b413d5cb
Jake Herrmann <jtherrmann@alaska.edu>
2023-05-22 09:51:38 -0800

    Update src/hyp3_isce2/logging.py
    Co-authored-by: Forrest Williams <31411324+forrestfwilliams@users.noreply.github.com>

--- src/hyp3_isce2/logging.py
+++ src/hyp3_isce2/logging.py
@@ -3,8 +3,9 @@ import sys
 
 
 def configure_root_logger() -> None:
-    # isce2 sets the root logger level to DEBUG, so we re-configure it here.
-    # See: https://github.com/ASFHyP3/hyp3-isce2/issues/53
+    """Re-configures the root level logger since ISCE2 sets it to DEBUG.
+    See: https://github.com/ASFHyP3/hyp3-isce2/issues/53 for context.
+    """
     logging.basicConfig(
         stream=sys.stdout,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
