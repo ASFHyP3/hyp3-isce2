9e298b1a2bfc638f1be26a105d0f52a7735aba4e
Forrest Williams <ffwilliams2@alaska.edu>
2023-05-23 17:06:11 -0500

    fix bperp

--- src/hyp3_isce2/insar_tops_burst.py
+++ src/hyp3_isce2/insar_tops_burst.py
@@ -10,6 +10,7 @@ from collections import namedtuple
 from pathlib import Path
 from shutil import copyfile, make_archive
 
+import numpy as np
 from hyp3lib.aws import upload_file_to_s3
 from hyp3lib.get_orb import downloadSentinelOrbitFile
 from hyp3lib.image import create_thumbnail
@@ -172,8 +173,8 @@ def make_parameter_file(
     slant_range_time = float(ref_annotation_xml.find('.//slantRangeTime').text)
     range_sampling_rate = float(ref_annotation_xml.find('.//rangeSamplingRate').text)
     number_samples = int(ref_annotation_xml.find('.//swathTiming/samplesPerBurst').text)
-    # FIXME: Not sure why baseline is negative
-    baseline_perp = topsProc_xml.find(f'.//IW-{swath_number}_Bperp_at_midrange_for_first_common_burst').text
+    # FIXME: Not sure why baseline is negative in some cases. I believe it is just a convention
+    baseline_perp = np.abs(topsProc_xml.find(f'.//IW-{swath_number}_Bperp_at_midrange_for_first_common_burst').text)
     unwrapper_type = topsApp_xml.find('.//property[@name="unwrapper name"]').text
     phase_filter_strength = topsApp_xml.find('.//property[@name="filter strength"]').text
 
