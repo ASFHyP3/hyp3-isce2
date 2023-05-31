aee042a6cb925b51584a696b34975d663cc022c9
jiangzhu <jzhu4@alaska.edu>
2023-05-31 15:16:42 -0800

    add code render the template readme.md.txt.j2

--- src/hyp3_isce2/metadata/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Package for generating HyP3 products' metadata"""
-
-from hyp3_isce2.metadata.create import create_metadata_file_set_insar, create_metadata_file_set_rtc
-
-__all__ = ['create_metadata_file_set_insar']
