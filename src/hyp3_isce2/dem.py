8f84482218b764869450ad39dd1cc5a18da8dd73
jiangzhu <jzhu4@alaska.edu>
2023-07-07 09:57:33 -0800

    make a function to return the correct extent that crossover the antimeridian

--- src/hyp3_isce2/dem.py
+++ src/hyp3_isce2/dem.py
@@ -72,17 +72,34 @@ def utm_from_lon_lat(lon: float, lat: float) -> int:
     return hemisphere + zone
 
 
-def get_dem_resolution(extent: list, res: float = 80.0):
+def get_correct_extent(extent):
+    """determine if the extent crossover antimeridian, return correct extent for extent that crossover antimeridian
+    Args:
+        extent: A list [xmin, ymin, xmax, ymax] for epsg:4326 (i.e. (x, y) = (lon, lat))
+    Returns:
+        correct extent which assures that longitude of the right side of extent
+        is larger than the longitude of the left side of the extent
+        """
+    if extent[0]*extent[2] < 0 and abs(extent[0]-extent[2]):
+        if extent[2] < 0:
+            extent[2] += 360.
+        if extent[0] < 0:
+            extent[0] += 360.
+    return extent
+
+
+def get_dem_resolution(extent: list, pixel_size: float = 80.0):
     """Calculate the pixel spacing in WGS84 for a given pixel spacing in UTM
     Args:
         extent: A list [xmin, ymin, xmax, ymax] for epsg:4326 (i.e. (x, y) = (lon, lat))
-        res: pixel spacing of output product
+        pixel_size: pixel spacing of output product
     Returns:
         The pixel spacing in WGS84 for the given pixel spacing in UTM
         """
     # coordinates of the center pixel
-    if extent[0] > 0.0 > extent[2]:
-        extent[2] += 360.0
+
+    # deal with extent crossing over antimeridian
+    extent = get_correct_extent(extent)
 
     lon_ul = (extent[0] + extent[2])/2.0
     lat_ul = (extent[1] + extent[3])/2.0
@@ -92,20 +109,15 @@ def get_dem_resolution(extent: list, res: float = 80.0):
     myprj2 = pyproj.Transformer.from_crs(epsg_code, 4326, always_xy=True)
 
     x_ul, y_ul = myprj1.transform(lon_ul, lat_ul)
-    lon_ur, lat_ur = myprj2.transform(x_ul + res, y_ul)
-    lon_ll, lat_ll = myprj2.transform(x_ul, y_ul - res)
-    lon_lr, lat_lr = myprj2.transform(x_ul + res, y_ul - res)
-
-    # adjust the lon values for the pixel crossing over antimeridian
-    if any(val > 0 for val in [lon_ul, lon_ll]) and any(val < 0 for val in [lon_ur, lon_lr]):
-
-        if lon_lr < 0:
-            lon_lr += 360.0
-        if lon_ur < 0:
-            lon_ur += 360
+    lon_ur, lat_ur = myprj2.transform(x_ul + pixel_size, y_ul)
+    lon_ll, lat_ll = myprj2.transform(x_ul, y_ul - pixel_size)
+    lon_lr, lat_lr = myprj2.transform(x_ul + pixel_size, y_ul - pixel_size)
 
     # envelope of the pixel in WGS84
     envelope = [min(lon_ul, lon_ll), min(lat_ll, lat_lr), max(lon_ur, lon_lr), max(lat_ul, lat_ur)]
+
+    # adjust the lon values for the pixel crossing over antimeridian
+    envelope = get_correct_extent(envelope)
 
     return max(abs(envelope[2]-envelope[0]), abs(envelope[3] - envelope[1]))
 
@@ -113,7 +125,7 @@ def get_dem_resolution(extent: list, res: float = 80.0):
 def download_dem_for_isce2(
         extent: list,
         dem_name: str = 'glo_30',
-        dem_res: float = 80.0,
+        pixel_size: float = 80.0,
         dem_dir: Path = None,
         buffer: float = .4) -> Path:
     """Download the given DEM for the given extent.
@@ -121,7 +133,7 @@ def download_dem_for_isce2(
     Args:
         extent: A list [xmin, ymin, xmax, ymax] for epsg:4326 (i.e. (x, y) = (lon, lat)).
         dem_name: One of the names from `dem_stitcher`.
-        dem_res: pixel spacing of output product
+        pixel_size: pixel spacing of output product
         dem_dir: The output directory.
         buffer: The extent buffer in degrees, by default .4, which is about 44 km at the equator
                 (or about 2.5 bursts at the equator).
@@ -138,7 +150,7 @@ def download_dem_for_isce2(
         dst_ellipsoidal_height=True,
         dst_area_or_point='Point',
         n_threads_downloading=5,
-        dst_resolution=get_dem_resolution(extent, dem_res)
+        dst_resolution=get_dem_resolution(extent, pixel_size)
     )
 
     dem_array[np.isnan(dem_array)] = 0.
