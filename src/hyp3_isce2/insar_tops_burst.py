cabf989fadd8d49e84c513c942a3cb85653cd714
Andrew Player <andrewplayer3@gmail.com>
2023-10-24 13:32:19 -0500

    Update src/hyp3_isce2/insar_tops_burst.py
    Co-authored-by: Jake Herrmann <jtherrmann@alaska.edu>

--- src/hyp3_isce2/insar_tops_burst.py
+++ src/hyp3_isce2/insar_tops_burst.py
@@ -200,7 +200,7 @@ def make_parameter_file(
         range_looks: int,
         dem_name: str = 'GLO_30',
         dem_resolution: int = 30,
-        apply_water_mask: bool = False) -> None:
+        apply_water_mask: bool) -> None:
     """Create a parameter file for the output product
 
     Args:
