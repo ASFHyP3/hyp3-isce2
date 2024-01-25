201e004c00301c85cf33427a3ff4668d3eab3a1b
jiangzhu <jzhu4@alaska.edu>
2024-01-24 15:28:40 -0900

    improve code style

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,30 +1,31 @@
 import os
 import shutil
-from pathlib import Path
 import filecmp
+from pathlib import Path
+
 
 import numpy as np
 import pytest
 from osgeo import gdal
 
 from hyp3_isce2.utils import (
-ESA_HOST,
-GDALConfigManager,
-create_image,
-extent_from_geotransform,
-get_esa_credentials,
-get_geotransform_from_dataset,
-image_math,
-isce2_copy,
-load_isce2_image,
-make_browse_image,
-oldest_granule_first,
-read_product_metadata,
-resample_to_radar,
-resample_to_radar_io,
-utm_from_lon_lat,
-write_isce2_image,
-write_isce2_image_from_obj,
+    ESA_HOST,
+    GDALConfigManager,
+    create_image,
+    extent_from_geotransform,
+    get_esa_credentials,
+    get_geotransform_from_dataset,
+    image_math,
+    isce2_copy,
+    load_isce2_image,
+    make_browse_image,
+    oldest_granule_first,
+    read_product_metadata,
+    resample_to_radar,
+    resample_to_radar_io,
+    utm_from_lon_lat,
+    write_isce2_image,
+    write_isce2_image_from_obj,
 )
 import isceobj
 
@@ -257,7 +258,7 @@ def test_write_isce2_image_from_obj(tmp_path):
     bands = 1
     length = 1
     width = array.shape[0]
-    out_path = str(tmp_path/'isce_image_1d')
+    out_path = str(tmp_path / 'isce_image_1d')
     _check_write_isce2_image_from_obj(out_path, bands, length, width)
 
     # 2D array, is shape(length, width), band=1
@@ -265,28 +266,28 @@ def test_write_isce2_image_from_obj(tmp_path):
     array = array.reshape(15, 10)
     bands = 1
     length, width = array.shape
-    out_path = str(tmp_path/'isce_image_2d')
+    out_path = str(tmp_path / 'isce_image_2d')
     _check_write_isce2_image_from_obj(out_path, bands, length, width)
 
     # multi-D array, its shape(band,length, width)
     array = np.array(range(150), dtype = np.float32)
     array = array.reshape(3, 5, 10)
     bands, length, width = array.shape
-    out_path = str(tmp_path/'isce_image_md')
+    out_path = str(tmp_path / 'isce_image_md')
     _check_write_isce2_image_from_obj(out_path, bands, length, width)
 
 
 def test_load_isce2_image(tmp_path):
-    in_path = str(tmp_path/'isce_image_md')
+    in_path = str(tmp_path / 'isce_image_md')
     arrayin = np.array(range(150), dtype = np.float32)
     arrayin = arrayin.reshape(3, 5, 10)
     bands, length, width = arrayin.shape
     write_isce2_image(in_path, array=arrayin, bands=bands, length=length, width=width, mode='write', data_type='FLOAT')
     image_obj, arrayout = load_isce2_image(in_path)
-    assert type(image_obj) == isceobj.Image.Image.Image
+    assert isinstance(image_obj, isceobj.Image.Image.Image)
     assert np.array_equal(arrayin, arrayout)
 
-    in_path = str(tmp_path/'isce_image_2d')
+    in_path = str(tmp_path / 'isce_image_2d')
 
     arrayin = np.array(range(150), dtype =  np.float32)
     arrayin = arrayin.reshape(15, 10)
@@ -297,7 +298,7 @@ def test_load_isce2_image(tmp_path):
     assert type(image_obj) == isceobj.Image.Image.Image
     assert np.array_equal(arrayin, arrayout)
 
-    in_path = str(tmp_path/'isce_image_1d')
+    in_path = str(tmp_path / 'isce_image_1d')
     arrayin = np.array(range(150), dtype=np.float32)
     arrayin = arrayin.reshape(1, 150)
     bands = 1
@@ -315,33 +316,33 @@ def test_get_geotransform_from_dataset():
 
 
 def test_isce2_copy(tmp_path):
-    in_path = str(tmp_path/'isce_image_2d')
+    in_path = str(tmp_path / 'isce_image_2d')
     arrayin = np.array(range(150), dtype=np.float32)
     arrayin = arrayin.reshape(15, 10)
     bands = 1
     length, width = arrayin.shape
     write_isce2_image(in_path, array=arrayin, bands=bands, length=length, width=width, mode='write', data_type='FLOAT')
-    out_path = str(tmp_path/'isce_image_2d_copy')
+    out_path = str(tmp_path / 'isce_image_2d_copy')
     isce2_copy(in_path, out_path)
     assert filecmp.cmp(in_path, out_path)
 
 
 def test_image_math(tmp_path):
-    in_path1 = str(tmp_path/'isce_image_2d_1')
+    in_path1 = str(tmp_path / 'isce_image_2d_1')
     array1 = np.array(range(150), dtype=np.float32)
     array1 = array1.reshape(15, 10)
     bands = 1
     length, width = array1.shape
     write_isce2_image(in_path1, array=array1, bands=bands, length=length, width=width, mode='write', data_type='FLOAT')
 
-    in_path2 = str(tmp_path/'isce_image_2d_2')
+    in_path2 = str(tmp_path / 'isce_image_2d_2')
     array2 = np.array(range(3, 153), dtype=np.float32)
     array2 = array2.reshape(15, 10)
     bands = 1
     length, width = array2.shape
     write_isce2_image(in_path2, array=array2, bands=bands, length=length, width=width, mode='write', data_type='FLOAT')
 
-    out_path = str(tmp_path/'isce_image_2d_out')
+    out_path = str(tmp_path / 'isce_image_2d_out')
     image_math(in_path1, in_path2, out_path, 'a + b')
     image_obj_out, arrayout = load_isce2_image(out_path)
     assert np.array_equal(array1 + array2, arrayout)
