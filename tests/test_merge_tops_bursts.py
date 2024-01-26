55d7692e81330de166d43b680ade418bb844726b
Forrest Williams <ffwilliams2@alaska.edu>
2024-01-26 12:50:23 -0600

    mild refactors

--- tests/test_merge_tops_bursts.py
+++ tests/test_merge_tops_bursts.py
@@ -1,14 +1,15 @@
 """Tests for hyp3_isce2.merge_tops_bursts module, use single quotes"""
 from datetime import datetime
-from pathlib import Path
 from unittest.mock import patch
 
 import asf_search
+import pytest
 
 import hyp3_isce2.burst as burst_utils
 import hyp3_isce2.merge_tops_bursts as merge
 
-# TODO combine with test_burst.py's version of same function
+
+# TODO combine with test_burst.py's version
 def mock_asf_search_results(
     slc_name: str, subswath: str, polarization: str, burst_index: int
 ) -> asf_search.ASFSearchResults:
@@ -28,60 +29,44 @@ def mock_asf_search_results(
     return results
 
 
-def test_to_burst_params():
-    product = merge.BurstProduct(
-        'granule',
-        datetime(2020, 1, 1),
-        datetime(2020, 1, 2),
-        666666,
-        'IW1',
-        'VV',
-        0,
-        Path('granule'),
-        100,
-        100,
-        10,
-        2,
-        10,
-        50,
-        20,
-        40,
-        0.1,
-        1,
-        datetime(2020, 1, 1, 0, 0, 1),
-        datetime(2020, 1, 2, 0, 0, 1),
-        99,
-    )
-    assert product.to_burst_params() == burst_utils.BurstParams('granule', 'IW1', 'VV', 0)
-
-
-def test_get_burst_metadata(test_merge_dir):
+@pytest.fixture
+def burst_product(test_merge_dir):
     product_path = list(test_merge_dir.glob('*'))[0]
-
-    with patch('hyp3_isce2.merge_tops_bursts.asf_search.granule_search') as mock_search:
-        mock_search.return_value = mock_asf_search_results('granule', 'IW1', 'VV', 1)
-        product = merge.get_burst_metadata([product_path])[0]
-
-    assert product == merge.BurstProduct(
+    product = merge.BurstProduct(
         granule='bar',
         reference_date=datetime(2020, 6, 4, 2, 23, 15),
         secondary_date=datetime(2020, 6, 16, 2, 23, 16),
         burst_id=0,
-        swath='IW1',
+        swath='IW2',
         polarization='VV',
         burst_number=1,
         product_path=product_path,
-        n_lines='377',
-        n_samples='1272',
+        n_lines=377,
+        n_samples=1272,
         range_looks=20,
         azimuth_looks=4,
-        first_valid_line='8',
-        n_valid_lines='363',
-        first_valid_sample='9',
-        n_valid_samples='1220',
-        az_time_interval='0.008222225199999992',
-        rg_pixel_size='46.59124229430646',
+        first_valid_line=8,
+        n_valid_lines=363,
+        first_valid_sample=9,
+        n_valid_samples=1220,
+        az_time_interval=0.008222225199999992,
+        rg_pixel_size=46.59124229430646,
         start_utc=datetime(2020, 1, 1, 0, 0),
         stop_utc=datetime(2020, 6, 4, 2, 23, 18, 795712),
         relative_orbit=1,
     )
+    return product
+
+
+def test_to_burst_params(burst_product):
+    assert burst_product.to_burst_params() == burst_utils.BurstParams('bar', 'IW2', 'VV', 1)
+
+
+def test_get_burst_metadata(test_merge_dir, burst_product):
+    product_path = list(test_merge_dir.glob('*'))[0]
+
+    with patch('hyp3_isce2.merge_tops_bursts.asf_search.granule_search') as mock_search:
+        mock_search.return_value = mock_asf_search_results('bar', 'IW2', 'VV', 1)
+        product = merge.get_burst_metadata([product_path])[0]
+
+    assert product == burst_product
