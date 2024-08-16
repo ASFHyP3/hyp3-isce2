227237d2cd9fed33b66476350b2f795e10e8df49
Mario Angarita <mf.angarita52@gmail.com>
2024-08-15 23:40:42 -0800

    Fixing water mask for insar_tops

---
+++ insar_tops.py
@@ -0,0 +1,211 @@
+"""Create a full SLC Sentinel-1 geocoded unwrapped interferogram using ISCE2's TOPS processing workflow"""
+
+import argparse
+import logging
+import os
+import sys
+from pathlib import Path
+from shutil import copyfile, make_archive
+
+from hyp3lib.util import string_is_true
+from s1_orbits import fetch_for_scene
+
+from hyp3_isce2 import packaging, slc, topsapp
+from hyp3_isce2.dem import download_dem_for_isce2
+from hyp3_isce2.logger import configure_root_logger
+from hyp3_isce2.s1_auxcal import download_aux_cal
+from hyp3_isce2.utils import make_browse_image
+
+
+log = logging.getLogger(__name__)
+
+
+def insar_tops(
+    reference_scene: str,
+    secondary_scene: str,
+    swaths: list = [1, 2, 3],
+    polarization: str = 'VV',
+    azimuth_looks: int = 4,
+    range_looks: int = 20,
+    apply_water_mask: bool = False,
+    download: bool = True,
+) -> Path:
+    """Create a full-SLC interferogram
+
+    Args:
+        reference_scene: Reference SLC name
+        secondary_scene: Secondary SLC name
+        swaths: Swaths to process
+        polarization: Polarization to use
+        azimuth_looks: Number of azimuth looks
+        range_looks: Number of range looks
+
+    Returns:
+        Path to the output files
+    """
+    orbit_dir = Path('orbits')
+    aux_cal_dir = Path('aux_cal')
+    dem_dir = Path('dem')
+
+    if download:
+        ref_dir = slc.get_granule(reference_scene)
+        sec_dir = slc.get_granule(secondary_scene)
+    else:
+        ref_dir = Path(reference_scene + '.SAFE')
+        sec_dir = Path(secondary_scene + '.SAFE')
+    roi = slc.get_dem_bounds(ref_dir, sec_dir)
+    log.info(f'DEM ROI: {roi}')
+
+    dem_path = download_dem_for_isce2(roi, dem_name='glo_30', dem_dir=dem_dir, buffer=0)
+    download_aux_cal(aux_cal_dir)
+
+    orbit_dir.mkdir(exist_ok=True, parents=True)
+    for granule in (reference_scene, secondary_scene):
+        log.info(f'Downloading orbit file for {granule}')
+        orbit_file = fetch_for_scene(granule, dir=orbit_dir)
+        log.info(f'Got orbit file {orbit_file} from s1_orbits')
+
+    config = topsapp.TopsappBurstConfig(
+        reference_safe=f'{reference_scene}.SAFE',
+        secondary_safe=f'{secondary_scene}.SAFE',
+        polarization=polarization,
+        orbit_directory=str(orbit_dir),
+        aux_cal_directory=str(aux_cal_dir),
+        dem_filename=str(dem_path),
+        geocode_dem_filename=str(dem_path),
+        roi=roi,
+        swaths=swaths,
+        azimuth_looks=azimuth_looks,
+        range_looks=range_looks,
+    )
+    config_path = config.write_template('topsApp.xml')
+
+    if apply_water_mask:
+        topsapp.run_topsapp_burst(start='startup', end='filter', config_xml=config_path)
+        water_mask_path = 'water_mask.wgs84'
+        create_water_mask(str(dem_path), water_mask_path)
+        multilook('merged/lon.rdr.full', outname='merged/lon.rdr', alks=azimuth_looks, rlks=range_looks)
+        multilook('merged/lat.rdr.full', outname='merged/lat.rdr', alks=azimuth_looks, rlks=range_looks)
+        resample_to_radar_io(water_mask_path, 'merged/lat.rdr', 'merged/lon.rdr', 'merged/water_mask.rdr')
+        isce2_copy('merged/phsig.cor', 'merged/unmasked.phsig.cor')
+        image_math('merged/unmasked.phsig.cor', 'merged/water_mask.rdr', 'merged/phsig.cor', 'a*b')
+        topsapp.run_topsapp_burst(start='unwrap', end='unwrap2stage', config_xml=config_path)
+        isce2_copy('merged/unmasked.phsig.cor', 'merged/phsig.cor')
+    else:
+        topsapp.run_topsapp_burst(start='startup', end='unwrap2stage', config_xml=config_path)
+    copyfile('merged/z.rdr.full.xml', 'merged/z.rdr.full.vrt.xml')
+    topsapp.run_topsapp_burst(start='geocode', end='geocode', config_xml=config_path)
+
+    return Path('merged')
+
+
+def insar_tops_packaged(
+    reference: str,
+    secondary: str,
+    swaths: list = [1, 2, 3],
+    polarization: str = 'VV',
+    azimuth_looks: int = 4,
+    range_looks: int = 20,
+    apply_water_mask: bool = True,
+    download: bool = True,
+    bucket: str = None,
+    bucket_prefix: str = '',
+) -> Path:
+    """Create a full-SLC interferogram
+
+    Args:
+        reference: Reference SLC name
+        secondary: Secondary SLC name
+        swaths: Swaths to process
+        polarization: Polarization to use
+        azimuth_looks: Number of azimuth looks
+        range_looks: Number of range looks
+        apply_water_mask: Apply water mask to unwrapped phase
+        download: Download the SLCs
+        bucket: AWS S3 bucket to upload the final product to
+        bucket_prefix: Bucket prefix to prefix to use when uploading the final product
+
+    Returns:
+        Path to the output files
+    """
+    pixel_size = packaging.get_pixel_size(f'{range_looks}x{azimuth_looks}')
+
+    log.info('Begin ISCE2 TopsApp run')
+    if os.path.exists(f'{reference}.SAFE') and os.path.exists(f'{secondary}.SAFE'):
+        insar_tops(reference, secondary, apply_water_mask=apply_water_mask, download=False)
+    else:
+        insar_tops(reference, secondary, apply_water_mask=apply_water_mask)
+    log.info('ISCE2 TopsApp run completed successfully')
+
+    product_name = packaging.get_product_name(reference, secondary, pixel_spacing=int(pixel_size))
+
+    product_dir = Path(product_name)
+    product_dir.mkdir(parents=True, exist_ok=True)
+
+    packaging.translate_outputs(product_name, pixel_size=pixel_size)
+
+    unwrapped_phase = f'{product_name}/{product_name}_unw_phase.tif'
+    if apply_water_mask:
+        packaging.water_mask(unwrapped_phase, f'{product_name}/{product_name}_water_mask.tif')
+
+    make_browse_image(unwrapped_phase, f'{product_name}/{product_name}_unw_phase.png')
+    packaging.make_readme(
+        product_dir=product_dir,
+        product_name=product_name,
+        reference_scene=reference,
+        secondary_scene=secondary,
+        range_looks=range_looks,
+        azimuth_looks=azimuth_looks,
+        apply_water_mask=apply_water_mask,
+    )
+    packaging.make_parameter_file(
+        Path(f'{product_name}/{product_name}.txt'),
+        reference_scene=reference,
+        secondary_scene=secondary,
+        azimuth_looks=azimuth_looks,
+        range_looks=range_looks,
+        apply_water_mask=apply_water_mask,
+    )
+    output_zip = make_archive(base_name=product_name, format='zip', base_dir=product_name)
+    if bucket:
+        packaging.upload_product_to_s3(product_dir, output_zip, bucket, bucket_prefix)
+
+
+def main():
+    """HyP3 entrypoint for the SLC TOPS workflow"""
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--reference', type=str, help='Reference granule')
+    parser.add_argument('--secondary', type=str, help='Secondary granule')
+    parser.add_argument('--polarization', type=str, default='VV', help='Polarization to use')
+    parser.add_argument(
+        '--looks', choices=['20x4', '10x2', '5x1'], default='20x4', help='Number of looks to take in range and azimuth'
+    )
+    parser.add_argument(
+        '--apply-water-mask',
+        type=string_is_true,
+        default=False,
+        help='Apply a water body mask before unwrapping.',
+    )
+    parser.add_argument('--bucket', help='AWS S3 bucket HyP3 for upload the final product(s)')
+    parser.add_argument('--bucket-prefix', default='', help='Add a bucket prefix to product(s)')
+
+    args = parser.parse_args()
+    configure_root_logger()
+    log.debug(' '.join(sys.argv))
+
+    range_looks, azimuth_looks = [int(looks) for looks in args.looks.split('x')]
+    if args.polarization not in ['VV', 'VH', 'HV', 'HH']:
+        raise ValueError('Polarization must be one of VV, VH, HV, or HH')
+
+    insar_tops_packaged(
+        reference=args.reference,
+        secondary=args.secondary,
+        polarization=args.polarization,
+        azimuth_looks=azimuth_looks,
+        range_looks=range_looks,
+        apply_water_mask=args.apply_water_mask,
+        bucket=args.bucket,
+        bucket_prefix=args.bucket_prefix,
+    )
+
+    log.info('ISCE2 TopsApp run completed successfully')
---
+++ insar_tops_burst.py
@@ -0,0 +1,306 @@
+"""Create a single-burst Sentinel-1 geocoded unwrapped interferogram using ISCE2's TOPS processing workflow"""
+
+import argparse
+import logging
+import sys
+from pathlib import Path
+from shutil import copyfile, make_archive
+from typing import Iterable, Optional
+
+import isce  # noqa
+from burst2safe.burst2safe import burst2safe
+from hyp3lib.util import string_is_true
+from isceobj.TopsProc.runMergeBursts import multilook
+from osgeo import gdal
+from s1_orbits import fetch_for_scene
+
+from hyp3_isce2 import packaging, topsapp
+from hyp3_isce2.burst import (
+    download_bursts,
+    get_burst_params,
+    get_isce2_burst_bbox,
+    get_region_of_interest,
+    multilook_radar_merge_inputs,
+    validate_bursts,
+)
+from hyp3_isce2.dem import download_dem_for_isce2
+from hyp3_isce2.insar_tops import insar_tops_packaged
+from hyp3_isce2.logger import configure_root_logger
+from hyp3_isce2.s1_auxcal import download_aux_cal
+from hyp3_isce2.utils import (
+    image_math,
+    isce2_copy,
+    make_browse_image,
+    oldest_granule_first,
+    resample_to_radar_io,
+)
+from hyp3_isce2.water_mask import create_water_mask
+
+
+gdal.UseExceptions()
+
+log = logging.getLogger(__name__)
+
+
+def insar_tops_burst(
+    reference_scene: str,
+    secondary_scene: str,
+    swath_number: int,
+    azimuth_looks: int = 4,
+    range_looks: int = 20,
+    apply_water_mask: bool = False,
+) -> Path:
+    """Create a burst interferogram
+
+    Args:
+        reference_scene: Reference burst name
+        secondary_scene: Secondary burst name
+        swath_number: Number of swath to grab bursts from (1, 2, or 3) for IW
+        azimuth_looks: Number of azimuth looks
+        range_looks: Number of range looks
+        apply_water_mask: Whether to apply a pre-unwrap water mask
+
+    Returns:
+        Path to results directory
+    """
+    orbit_dir = Path('orbits')
+    aux_cal_dir = Path('aux_cal')
+    dem_dir = Path('dem')
+
+    ref_params = get_burst_params(reference_scene)
+    sec_params = get_burst_params(secondary_scene)
+
+    ref_metadata, sec_metadata = download_bursts([ref_params, sec_params])
+
+    is_ascending = ref_metadata.orbit_direction == 'ascending'
+    ref_footprint = get_isce2_burst_bbox(ref_params)
+    sec_footprint = get_isce2_burst_bbox(sec_params)
+
+    insar_roi = get_region_of_interest(ref_footprint, sec_footprint, is_ascending=is_ascending)
+    dem_roi = ref_footprint.intersection(sec_footprint).bounds
+
+    if abs(dem_roi[0] - dem_roi[2]) > 180.0 and dem_roi[0] * dem_roi[2] < 0.0:
+        raise ValueError('Products that cross the anti-meridian are not currently supported.')
+
+    log.info(f'InSAR ROI: {insar_roi}')
+    log.info(f'DEM ROI: {dem_roi}')
+
+    dem_path = download_dem_for_isce2(dem_roi, dem_name='glo_30', dem_dir=dem_dir, buffer=0, resample_20m=False)
+    download_aux_cal(aux_cal_dir)
+
+    if range_looks == 5:
+        geocode_dem_path = download_dem_for_isce2(
+            dem_roi, dem_name='glo_30', dem_dir=dem_dir, buffer=0, resample_20m=True
+        )
+    else:
+        geocode_dem_path = dem_path
+
+    orbit_dir.mkdir(exist_ok=True, parents=True)
+    for granule in (ref_params.granule, sec_params.granule):
+        log.info(f'Downloading orbit file for {granule}')
+        orbit_file = fetch_for_scene(granule, dir=orbit_dir)
+        log.info(f'Got orbit file {orbit_file} from s1_orbits')
+
+    config = topsapp.TopsappBurstConfig(
+        reference_safe=f'{ref_params.granule}.SAFE',
+        secondary_safe=f'{sec_params.granule}.SAFE',
+        polarization=ref_params.polarization,
+        orbit_directory=str(orbit_dir),
+        aux_cal_directory=str(aux_cal_dir),
+        roi=insar_roi,
+        dem_filename=str(dem_path),
+        geocode_dem_filename=str(geocode_dem_path),
+        swaths=swath_number,
+        azimuth_looks=azimuth_looks,
+        range_looks=range_looks,
+    )
+    config_path = config.write_template('topsApp.xml')
+
+    topsapp.run_topsapp_burst(start='startup', end='preprocess', config_xml=config_path)
+    topsapp.swap_burst_vrts()
+    if apply_water_mask:
+        topsapp.run_topsapp_burst(start='computeBaselines', end='filter', config_xml=config_path)
+        water_mask_path = 'water_mask.wgs84'
+        create_water_mask(str(dem_path), water_mask_path)
+        multilook('merged/lon.rdr.full', outname='merged/lon.rdr', alks=azimuth_looks, rlks=range_looks)
+        multilook('merged/lat.rdr.full', outname='merged/lat.rdr', alks=azimuth_looks, rlks=range_looks)
+        resample_to_radar_io(water_mask_path, 'merged/lat.rdr', 'merged/lon.rdr', 'merged/water_mask.rdr')
+        isce2_copy('merged/phsig.cor', 'merged/unmasked.phsig.cor')
+        image_math('merged/unmasked.phsig.cor', 'merged/water_mask.rdr', 'merged/phsig.cor', 'a*b')
+        topsapp.run_topsapp_burst(start='unwrap', end='unwrap2stage', config_xml=config_path)
+        isce2_copy('merged/unmasked.phsig.cor', 'merged/phsig.cor')
+    else:
+        topsapp.run_topsapp_burst(start='computeBaselines', end='unwrap2stage', config_xml=config_path)
+    copyfile('merged/z.rdr.full.xml', 'merged/z.rdr.full.vrt.xml')
+    topsapp.run_topsapp_burst(start='geocode', end='geocode', config_xml=config_path)
+
+    return Path('merged')
+
+
+def insar_tops_single_burst(
+    reference: str,
+    secondary: str,
+    looks: str = '20x4',
+    apply_water_mask=False,
+    bucket: Optional[str] = None,
+    bucket_prefix: str = '',
+):
+    reference, secondary = oldest_granule_first(reference, secondary)
+    validate_bursts(reference, secondary)
+    swath_number = int(reference[12])
+    range_looks, azimuth_looks = [int(looks) for looks in looks.split('x')]
+
+    log.info('Begin ISCE2 TopsApp run')
+
+    insar_tops_burst(
+        reference_scene=reference,
+        secondary_scene=secondary,
+        azimuth_looks=azimuth_looks,
+        range_looks=range_looks,
+        swath_number=swath_number,
+        apply_water_mask=apply_water_mask,
+    )
+
+    log.info('ISCE2 TopsApp run completed successfully')
+
+    multilook_position = multilook_radar_merge_inputs(swath_number, rg_looks=range_looks, az_looks=azimuth_looks)
+
+    pixel_size = packaging.get_pixel_size(looks)
+    product_name = packaging.get_product_name(reference, secondary, pixel_spacing=int(pixel_size), slc=False)
+
+    product_dir = Path(product_name)
+    product_dir.mkdir(parents=True, exist_ok=True)
+
+    packaging.translate_outputs(product_name, pixel_size=pixel_size, include_radar=True, use_multilooked=True)
+
+    unwrapped_phase = f'{product_name}/{product_name}_unw_phase.tif'
+    if apply_water_mask:
+        packaging.water_mask(unwrapped_phase, f'{product_name}/{product_name}_water_mask.tif')
+
+    make_browse_image(unwrapped_phase, f'{product_name}/{product_name}_unw_phase.png')
+
+    packaging.make_readme(
+        product_dir=product_dir,
+        product_name=product_name,
+        reference_scene=reference,
+        secondary_scene=secondary,
+        range_looks=range_looks,
+        azimuth_looks=azimuth_looks,
+        apply_water_mask=apply_water_mask,
+    )
+    packaging.make_parameter_file(
+        Path(f'{product_name}/{product_name}.txt'),
+        reference_scene=reference,
+        secondary_scene=secondary,
+        azimuth_looks=azimuth_looks,
+        range_looks=range_looks,
+        multilook_position=multilook_position,
+        apply_water_mask=apply_water_mask,
+    )
+    output_zip = make_archive(base_name=product_name, format='zip', base_dir=product_name)
+
+    if bucket:
+        packaging.upload_product_to_s3(product_dir, output_zip, bucket, bucket_prefix)
+
+
+def insar_tops_multi_burst(
+    reference: Iterable[str],
+    secondary: Iterable[str],
+    swaths: list = [1, 2, 3],
+    looks: str = '20x4',
+    apply_water_mask=False,
+    bucket: Optional[str] = None,
+    bucket_prefix: str = '',
+):
+    ref_ids = [g.split('_')[1] + '_' + g.split('_')[2] + '_' + g.split('_')[4] for g in reference]
+    sec_ids = [g.split('_')[1] + '_' + g.split('_')[2] + '_' + g.split('_')[4] for g in secondary]
+
+    if len(list(set(ref_ids) - set(sec_ids))) > 0:
+        raise Exception(
+            'The reference bursts '
+            + ', '.join(list(set(ref_ids) - set(sec_ids)))
+            + ' do not have the correspondant bursts in the secondary granules'
+        )
+    elif len(list(set(sec_ids) - set(ref_ids))) > 0:
+        raise Exception(
+            'The secondary bursts '
+            + ', '.join(list(set(sec_ids) - set(ref_ids)))
+            + ' do not have the correspondant bursts in the reference granules'
+        )
+
+    if not reference[0].split('_')[4] == secondary[0].split('_')[4]:
+        raise Exception('The secondary and reference granules do not have the same polarization')
+
+    reference_safe_path = burst2safe(reference)
+    reference_safe = reference_safe_path.name.split('.')[0]
+    secondary_safe_path = burst2safe(secondary)
+    secondary_safe = secondary_safe_path.name.split('.')[0]
+
+    range_looks, azimuth_looks = [int(looks) for looks in looks.split('x')]
+    swaths = list(set([int(granule.split('_')[2][2]) for granule in reference]))
+    polarization = reference[0].split('_')[4]
+
+    log.info('Begin ISCE2 TopsApp run')
+    insar_tops_packaged(
+        reference=reference_safe,
+        secondary=secondary_safe,
+        swaths=swaths,
+        polarization=polarization,
+        azimuth_looks=azimuth_looks,
+        range_looks=range_looks,
+        apply_water_mask=apply_water_mask,
+        bucket=bucket,
+        bucket_prefix=bucket_prefix
+    )
+    log.info('ISCE2 TopsApp run completed successfully')
+
+
+def main():
+    """HyP3 entrypoint for the burst TOPS workflow"""
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--bucket', help='AWS S3 bucket HyP3 for upload the final product(s)')
+    parser.add_argument('--bucket-prefix', default='', help='Add a bucket prefix to product(s)')
+    parser.add_argument(
+        '--looks', choices=['20x4', '10x2', '5x1'], default='20x4', help='Number of looks to take in range and azimuth'
+    )
+    parser.add_argument(
+        '--apply-water-mask',
+        type=string_is_true,
+        default=False,
+        help='Apply a water body mask before unwrapping.',
+    )
+    # Allows granules to be given as a space-delimited list of strings (e.g. foo bar) or as a single
+    # quoted string that contains spaces (e.g. "foo bar"). AWS Batch uses the latter format when
+    # invoking the container command.
+    parser.add_argument('--reference', type=str.split, nargs='+', help='List of granules for the reference bursts')
+    parser.add_argument('--secondary', type=str.split, nargs='+', help='List of granules for the secondary bursts')
+
+    args = parser.parse_args()
+
+    args.reference = [item for sublist in args.reference for item in sublist]
+    args.secondary = [item for sublist in args.secondary for item in sublist]
+    if len(args.reference) != len(args.secondary):
+        parser.error('Number of reference and secondary granules must be the same')
+
+    configure_root_logger()
+    log.debug(' '.join(sys.argv))
+
+    if len(args.reference) == 1:
+        insar_tops_single_burst(
+            reference=args.reference[0],
+            secondary=args.secondary[0],
+            looks=args.looks,
+            apply_water_mask=args.apply_water_mask,
+            bucket=args.bucket,
+            bucket_prefix=args.bucket_prefix,
+        )
+    else:
+        insar_tops_multi_burst(
+            reference=args.reference,
+            secondary=args.secondary,
+            looks=args.looks,
+            apply_water_mask=args.apply_water_mask,
+            bucket=args.bucket,
+            bucket_prefix=args.bucket_prefix,
+        )
