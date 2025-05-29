24ff6f5ecc7a20775dbbfa84b79e96f33d2be046
mfangaritav <mf.angarita52@gmail.com>
2025-05-29 15:18:38 -0800

    Update src/hyp3_isce2/utils.py
    Co-authored-by: Joseph H Kennedy <me@jhkennedy.org>

--- src/hyp3_isce2/utils.py
+++ src/hyp3_isce2/utils.py
@@ -168,10 +168,10 @@ def make_browse_image(input_tif: str, output_png: str) -> None:
         try:
             stats = gdal.Info(input_tif, format='json', stats=True)['stac']['raster:bands'][0]['stats']
         except RuntimeError as error:
-            if 'no valid pixels' in str(error):
-                stats = {'minimum': 0, 'maximum': 0}
-            else:
+            if 'no valid pixels' not in str(error):
                 raise
+            stats = {'minimum': 0, 'maximum': 0}
+
         gdal.Translate(
             destName=output_png,
             srcDS=input_tif,
